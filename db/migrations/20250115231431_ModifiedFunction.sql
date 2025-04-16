9476a2dfb8b67e4c22d37491e0e2ba17a5d0b7c1
Robert Rees <shudderfix@gmail.com>
2025-04-16 23:27:05 +0100

    Corrects the name of the column

--- db/migrations/20250115231431_ModifiedFunction.sql
+++ db/migrations/20250115231431_ModifiedFunction.sql
@@ -3,7 +3,7 @@
 CREATE OR REPLACE FUNCTION update_modified_column() 
 	RETURNS TRIGGER AS $$
 	BEGIN
-	    NEW.modified = now();
+	    NEW.updated = now();
 	    RETURN NEW; 
 	END;
 	$$ language 'plpgsql';

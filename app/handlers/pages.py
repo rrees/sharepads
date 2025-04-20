280885765cfc1fdb994649ab102fcac5ae7871dc
Robert Rees <shudderfix@gmail.com>
2025-04-20 23:03:38 +0100

    Renders a sharepad if the slug is matched

--- app/handlers/pages.py
+++ app/handlers/pages.py
@@ -1,4 +1,17 @@
 import flask
+import werkzeug
+
+from app.repositories import sharepads as sharepad_repository
+
 
 def front_page():
-	return flask.render_template('index.html')
+    return flask.render_template("index.html")
+
+
+def sharepad(slug):
+    sharepad = sharepad_repository.get_by_slug(slug)
+
+    if not sharepad:
+        raise werkzeug.exceptions.NotFound("Sharepad is unknown")
+
+    return flask.render_template("sharepad.html", sharepad=sharepad)
--- app/handlers/routes.py
+++ app/handlers/routes.py
@@ -2,4 +2,5 @@ from . import pages
 
 routes = [
     ("/", "index", pages.front_page, ["GET"]),
+    ("/sharepad/<slug>", "sharepad", pages.sharepad, ["GET"]),
 ]
